16812160fe17b269de8d008b94f8103746d79603
Guan-Yu-Chen <155933115+Guan-Yu-Chen@users.noreply.github.com>
2024-07-03 11:19:10 +0800

    Create Week5_2.cpp
    Create Week5_2.cpp

---
+++ Week5_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int h, i, j;
+    cout << "Please input the height of your pyramid:";
+    cin >> h;
+
+    for(i = 1 ; i <= h ; i++)
+    {
+        for(j = h-i ; j > 0 ; j--)
+        {
+            cout << " ";
+        }
+
+        for(j = i*2-1 ; j > 0 ; j--)
+        {
+            cout << "X";
+        }
+
+        cout << "\n";
+    }
+}
